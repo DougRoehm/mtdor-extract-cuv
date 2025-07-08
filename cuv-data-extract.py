2fcae27b9d888e36b26cb5f82cf670bf188cd083
Doug Roehm <158538535+DougRoehm@users.noreply.github.com>
2025-07-07 18:31:40 -0600

    Code to extract and save CUV pg working
    Still need to build out processing more than one file at a time.
    I am envisioning putting all files needing to be processed in a
    folder and then the program will open each file extract the CUV
    page and save in one workbook.

--- cuv-data-extract.py
+++ cuv-data-extract.py
@@ -1,42 +1,27 @@
-# Import file
+# Program to extract CUV worksheet from Excel appraisal workbook
+
+
 # Import modules and set parameters
 import pandas as pd
 import numpy as np
 import os
 
 
-# Set parameters
-pd.set_option('display.max_rows', None)
-
-
 # Use predefined path and file for now but will need to allow for user to select/input this info
-folder_path = '/Users/dougroehm/Code/repos/mtdor-extract-cuv/TY23-cap-final-appraisals/'
+folder_path = '/Users/dougroehm/Code/repos/mtdor-extract-cuv/TY23-cap-final-appraisals'
 file_name = 'U202_TY23_CAP_Final_Appraisal.xlsx'
-file_path = folder_path + file_name
-
-
-# Create a Pandas DataFrame from file
-# df = pd.ExcelFile(file_path)
+file_path = folder_path + "/" + file_name
 
 
 # Create a Pandas DataFrame of the "CUV" worksheet
-df = pd.read_excel(file_path, sheet_name='CUV', header=None, usecols="A:D", names=["Col1", "Col2", "Col3", "Col4"])
+df = pd.read_excel(file_path, sheet_name='CUV', header=None, usecols="A:D", index_col=None)
 
 
-# Use .notna to remove rows that are blank
-df_notna = df[df['Col4'].notna()]
+# Extract company name and first word to include in worksheet name
+company_name = df.iloc[1, 0]
+co_first_word = company_name.split()[0]
 
 
-# Extract the rows to be kept
-df_to_extract = df_notna[df_notna['Col1'].str.contains("Cost") | 
-df_notna['Col1'].str.contains("Capitalization") | 
-df_notna['Col1'].str.contains("Stock") |
-df_notna['Col1'].str.contains("Correlated Unit Value") |
-df_notna['Col1'].str.contains("Montana Allocation Factor") |
-df_notna['Col1'].str.contains("Montana Allocated Value") |
-df_notna['Col1'].str.contains("Value to be Apportioned")
-]
-
-print(df_to_extract)
-
-# I am going to need to pull the data using iloc because there is too much overlap in string names
+# Write data to new Excel workbook
+df.to_excel("cuv-extract.xlsx", sheet_name=co_first_word + "_CUV", header=False, index=False)
+print("Done")
